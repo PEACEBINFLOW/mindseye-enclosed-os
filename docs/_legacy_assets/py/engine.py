4bc967f1f7999e5429d52667271644202f354d18
Peace Thabiwa <peacethabibinflow@proton.me>
2026-01-05 01:12:06 +0100

    Create engine.py

---
+++ docs/_legacy_assets/py/engine.py
@@ -0,0 +1,10 @@
+"""
+Legacy engine (deprecated)
+
+The active engine lives in:
+  /docs/os/engine/py/engine.py
+"""
+
+class LegacyEngine:
+    def __init__(self):
+        self.status = "deprecated"
