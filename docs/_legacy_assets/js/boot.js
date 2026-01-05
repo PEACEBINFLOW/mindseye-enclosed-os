81fef41344d548f88b514e70f79a91a86a1dc6ff
Peace Thabiwa <peacethabibinflow@proton.me>
2026-01-05 01:11:01 +0100

    Create boot.js

---
+++ docs/_legacy_assets/js/boot.js
@@ -0,0 +1,8 @@
+/**
+ * Legacy boot file (deprecated)
+ * Replaced by:
+ *   /docs/os/router/router.js
+ *   /docs/os/engine/boot/pyodide_boot.js
+ */
+
+console.warn("Legacy boot.js loaded (should not happen)");
